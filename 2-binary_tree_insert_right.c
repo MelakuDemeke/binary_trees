a66c7374d06d3e698c38b674f021176ab71b84dc
MelakuDemeke <melaku.demeke789@gmail.com>
2023-05-30 15:54:31 +0300

    binary tree define

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right - Inserts a node as the right child of a given node
+ * @parent: Pointer to the node to insert the right child in.
+ * @value: The value to store in the new node.
+ *
+ * Return: Pointer to the newly created node, or NULL on failure.
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node = NULL;
+
+	if (parent == NULL || value == '\0')
+		return (NULL);
+
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
+
+
+}
