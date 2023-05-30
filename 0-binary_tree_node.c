e03f1c6394d822f0dee5feb1e3efbd65a66cb646
MelakuDemeke <melaku.demeke789@gmail.com>
2023-05-30 15:00:31 +0300

    null and empty check

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -8,3 +8,15 @@
  * Return: Pointer to the newly created node
  */
 
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
+
+	if (value == '\0')
+		return (NULL);
+
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+		return (NULL);
+
+}
