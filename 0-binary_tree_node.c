fbd0b714b56278ee335101c508680d9afe01c01b
MelakuDemeke <melaku.demeke789@gmail.com>
2023-05-30 14:59:36 +0300

    add the doc

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,10 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - Creates a binary tree node
+ * @parent: Pointer to the parent binary tree node
+ * @value: Value to be assigned to the new node
+ *
+ * Return: Pointer to the newly created node
+ */
+
